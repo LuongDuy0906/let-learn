9c02b23b9f47ed78a9c895e637ad840c10d01a53
luongduy0906 <luongducduy0906@gmail.com>
2026-03-08 13:17:51 +0700

    them validator de validate du lieu

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,1 +1,12 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty } from "class-validator";
+
+export class CreateUserDto {
+    username?: string;
+
+    @IsNotEmpty({message: "Email khong duoc de trong"})
+    @IsEmail({}, {message: "Email khong dung dinh dang"})
+    email!: string;
+
+    @IsNotEmpty({message: "Mat khau khong duoc de trong"})
+    password!: string;
+}
